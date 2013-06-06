8996d25c758f3ef36cef5b0ac74d235e025ddbb9
PLANET-SOFT <git.planetsoft@gmail.com>
2013-06-06 16:10:26 +0100

    the first commit

---
+++ test4/src/com/planetsoft/service/Main.java
@@ -0,0 +1,12 @@
+package com.planetsoft.service;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+           System.out.println("hello world");
+	}
+
+}
