48b95c70dea13e3ad1969d132a37a70713bd3aa8
PLANET-SOFT <git.planetsoft@gmail.com>
2013-06-06 17:15:32 +0000

    LAST COMMIT FOR TODAY

--- test4/src/com/planetsoft/service/Main.java
+++ test4/src/com/planetsoft/service/Main.java
@@ -10,6 +10,8 @@ public class Main {
            System.out.println("this the second modif and commit");
            System.out.println("the third commit");
            System.out.println("this the fourth modif and commit");
+           System.out.println("this the 7 modif and commit");
+
 
 	}
 
